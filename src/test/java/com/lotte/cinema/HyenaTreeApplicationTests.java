dd034d329ddfa913a95ac2578eeba4ef164f354c
Ethan-SY <kangie3@naver.com>
2024-06-26 10:13:20 +0900

    첫 만남(우연)

---
+++ src/test/java/com/lotte/cinema/HyenaTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lotte.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyenaTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
